9ed39625a968f98562393a30242b5b3d06c7e366
Truman Arneld <tarneld@oa-k3.msk.21-school.ru>
2020-11-05 21:16:25 +0300

    1try-2

--- get_next_line.c
+++ get_next_line.c
@@ -14,40 +14,43 @@
 
 int get_next_line(int fd, char **line)
 {
-	static char	*s[100];
+	static char	*s[255];
+	int			n;
 	char		*temp;
 	char		w[BUFFER_SIZE + 1];
 	int			j;
-	int			n;
 	//int			t;
 
 	if (fd < 0)
 		return (-1);
 	temp = NULL;
+	j = 0;
+	if (*s != NULL)
+		while (*s[j] != '\n' || *s[j] != '\0')
+			j++;
+	if (*s[j] != '\n' || *s[j] != '\0')
+		free(*s);
+		//*s = temp;
+		//free (&temp);
+		////printf("it's\t15\n");
+		//j = ft_strsize(w, j);
+		////printf("it's\t16\n");
+		
+		//temp = ft_strjoinsize(*s, w, ft_strsize(w, j));
+	}
+	
 	if ((n = read(fd, w, BUFFER_SIZE)) > 0)
 	{
 		j = 0;
 		w[n] = '\0';
 		//printf("it's\t1\n");
-		if (*s != NULL)
-		{
-			printf("it's\t11\n");
-			temp = ft_strjoinsize(*s, w, ft_strsize(w, j));
-			free(s);
-			j = ft_strsize(w, j);
-		}
-		//printf("it's\t2\n");
-		while (w[ft_strsize(w, j)] != '\0')
-		{
-			*line = ft_newstr(w, j);
-			printf("it's while - %s - %i\n", *line, j);
-			j = ft_strsize(w, j) + 1;
-			printf("it's end - |%c| %i\n", w[j], j);
-			line++;
-		}
-		//printf("it's\t3 - %s\n", *s);
-		*s = ft_newstr(w, j);
-		printf("it's\t4 %s\n", *s);
+		*line = ft_newstr(w, j);
+		printf("it's while - %s - %i\n", *line, j);
+		j = ft_strsize(w, j) + 1;
+		printf("it's end - |%c| %i\n", w[j], j);
+		if (w[j] != '\0')
+			*s = ft_newstr(w, j);
+		printf("it's\tnext %s\n", *s);
 	}
 	return ((n == 0) ? 0 : 1);
 }
